54855be6e01fee810bdbd7eacd54d552092001b3
doquockhanh <dqkhanh293@gmail.com>
2024-10-20 13:00:37 +0700

    up 3

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Product = require("./models/Product");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
